78e10e48bf0ef358dcc7fc6ce02b3ada1993cb80
versecafe <147033096+versecafe@users.noreply.github.com>
2024-08-19 03:15:26 -0500

    (assignment) & (comparison)

--- grammar.js
+++ grammar.js
@@ -15,7 +15,8 @@ module.exports = grammar({
         ),
       ),
     comment: ($) => token(seq("#", /.*/)),
-    variable_assignment: ($) => seq($.varname, "=", $.value),
+    variable_assignment: ($) => seq($.varname, $.assignment, $.value),
+    assignment: ($) => "=",
     object_assignment: ($) =>
       seq($.varname, "=", "{", repeat($.key_value_pair), "}"),
     key_value_pair: ($) => seq($.varname, "=", $.value),
@@ -25,7 +26,7 @@ module.exports = grammar({
     string: ($) => seq('"', /[^"\n]*/, '"'), // Matches strings enclosed in double quotes
     boolean: ($) => choice("yes", "no"), // Matches boolean values
     eol: ($) => choice($.comment, "\n"), // Matches end of line with optional comment
-    comparison_operator: ($) => choice("<=", ">="),
+    comparison: ($) => choice("<=", ">="),
     comparison_operation: ($) =>
       choice(seq($.varname, $.comparison_operator, $.value)),
   },
